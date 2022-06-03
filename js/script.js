daad2cb89ae4a37eaae77a4b37e376c48a2a4ec3
kevin <kevincaldieraro123@gmail.com>
2022-06-03 05:29:51 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let btnHamburguer = document.querySelector(".hamburguer");
+let nav = document.querySelector("#nav");
+
+btnHamburguer.addEventListener("click", () => {
+
+    btnHamburguer.classList.toggle("active");
+    nav.classList.toggle("active");
+
+});
